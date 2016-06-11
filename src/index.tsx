0228438f87b304141a9c25f5d8d8c77927a622ae
Simone Mulas <simone.mulas@gmail.com>
2016-06-11 12:42:18 +0200

    sm

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-export {GlyphYesNo} from './lib/misc/misc';
+export {AjaxPicker,AjaxPickerProps} from './lib/ajaxPicker';
+export {AutoSaveCheckBox,AutoSaveCheckBoxProps} from './lib/autoSaveCheckBox';
+export {DeleteButton,DeleteButtonProps} from './lib/deleteButton';
+export {EditableH1,EditableH1Props} from './lib/editableH1';
+export {GlyphYesNo} from './lib/misc';
+
+
--- src/lib/misc/misc.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as BS from 'react-bootstrap';
-
-const colorForSwitch = (value:boolean)=> ({color: value?'green':'lightgray'});
-
-export const GlyphYesNo = (props:{value:boolean}) =>{
-    return <BS.Glyphicon style={colorForSwitch(props.value)} glyph={props.value ? 'ok-sign' :'ban-circle'} />
-}
