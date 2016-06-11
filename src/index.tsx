3598d9b9d03fcb8f5f4d54f780a5d4533f324990
Simone Mulas <simone.mulas@gmail.com>
2016-06-11 12:48:51 +0200

    added autosave control

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export {AjaxPicker,AjaxPickerProps} from './lib/ajaxPicker';
 export {AutoSaveCheckBox,AutoSaveCheckBoxProps} from './lib/autoSaveCheckBox';
+export {AutoSaveControl,AutoSaveControlProps} from './lib/autoSaveControl';
 export {DeleteButton,DeleteButtonProps} from './lib/deleteButton';
 export {EditableH1,EditableH1Props} from './lib/editableH1';
 export {GlyphYesNo} from './lib/misc';
